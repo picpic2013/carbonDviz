cffb2385650676516050f35b655e4f4298755a41
pic_Gigabyte <picpic2019@gmail.com>
2021-05-14 17:44:34 +0800

    fix Asynchronous update bug

--- js/utils/SceneBase.js
+++ js/utils/SceneBase.js
@@ -1,177 +1,209 @@
-class SceneBase {
-    constructor (conf, father) {
-        this.__subObjects__ = this.__subObjects__ ? this.__subObjects__ : {}
-        this.__father__ = (father === undefined) ? window : father
-        this.__isActive__ = false
-        this.__start__ = 0
-        this.__end__ = 1
+function SceneBase(conf, father, ...subObjs) {
+    this.__subObjects__ = this.__subObjects__ ? this.__subObjects__ : {}
+    this.__father__ = (father === undefined) ? window : father
+    this.__isActive__ = false
+    this.__start__ = 0
+    this.__end__ = 1
 
-        // 如果传入的参数是数组，就递归生成子元素
-        if (Array.isArray(conf)) {
-            for (var subObj of conf) {
-                this.addSubObject(subObj, undefined, father)
-            }
-            return
+    // 如果传入的参数是数组，就递归生成子元素
+    if (Array.isArray(conf)) {
+        for (var subObj of conf) {
+            this.addSubObject(subObj, undefined, father)
         }
+        return
+    }
 
-        // 将类中的子动画全部转换为 SceneBase 类
-        var tmpSubObjs = this.__subObjects__
-        this.__subObjects__ = {}
-        if (Array.isArray(tmpSubObjs)) {
-            for (var value of tmpSubObjs) {
+    // 将类中的子动画全部转换为 SceneBase 类
+    var tmpSubObjs = this.__subObjects__
+    this.__subObjects__ = {}
+    if (Array.isArray(tmpSubObjs)) {
+        for (var value of tmpSubObjs) {
+            this.addSubObject(value)
+        }
+    } else if (tmpSubObjs instanceof SceneBase) {
+        this.addSubObject(tmpSubObjs)
+    } else {
+        for (var key of Object.keys(tmpSubObjs)) {
+            this.addSubObject(tmpSubObjs[key], key)
+        }
+    }
+
+    // 默认初始化 conf
+    conf = (conf === undefined) ? {} : conf  
+
+    // 把 conf 里所有的属性都转移到对象中
+    Object.assign(this, conf, {
+        "__onUpdate__": this.__onUpdate__, 
+        "__onInactive__": this.__onInactive__, 
+        "__subObjects__": this.__subObjects__
+    })
+    
+    for (var sub of subObjs) {
+        this.addSubObject(sub)
+    }
+
+    // 单独处理三个变量
+    if (conf instanceof SceneBase) {
+        this.__onUpdate__ = conf.__onUpdate__
+    } else if (conf.__onUpdate__) {
+        this.__onUpdateFunction__ = conf.__onUpdate__
+    }
+
+    if (conf instanceof SceneBase) {
+        this.__onInactive__ = conf.__onInactive__
+    } else if (conf.__onInactive__) {
+        this.__onInactiveFunction__ = conf.__onInactive__
+    }
+
+    if (conf.__subObjects__) {
+        if (Array.isArray(conf.__subObjects__)) {
+            for (var value of conf.__subObjects__) {
                 this.addSubObject(value)
             }
-        } else if (tmpSubObjs instanceof SceneBase) {
-            this.addSubObject(tmpSubObjs)
         } else {
-            for (var key of Object.keys(tmpSubObjs)) {
-                this.addSubObject(tmpSubObjs[key], key)
+            for (var key of Object.keys(conf.__subObjects__)) {
+                this.addSubObject(conf.__subObjects__[key], key)
             }
         }
+    }
 
-        // 默认初始化 conf
-        conf = (conf === undefined) ? {} : conf  
+    // 添加触发器
+    if (conf instanceof SceneBase) {
+        if (conf.__triggers__ === undefined) {
+            conf.__triggers__ = []
+        }
+        conf.__triggers__.push(this)
+    }
+}
+
+SceneBase.prototype.__isInActiveRange__ = function (rate, scrolled, gloalVars) {
+    if (rate < this.__start__ || rate > this.__end__) {
+        return false
+    }
+    return true
+}
+
+SceneBase.prototype.__calculatePercentage__ = function (obj, rate, scrolled, gloalVars) {
+    return (rate - obj.__start__) / (obj.__end__ - obj.__start__)
+}
+
+SceneBase.prototype.__onActivate__ = function (rate, abso, gloalVars) {
+    if (this.__onActivateFunction__) {
+        this.__onActivateFunction__.call(this, rate, abso, gloalVars)
+    }
+}
+
+SceneBase.prototype.__onUpdate__ = function (rate, scrolled, gloalVars) {
+    // 先自己更新
+    if (this.__onUpdateFunction__) {
+        this.__onUpdateFunction__.call(this, rate, scrolled, gloalVars)
+    }
+
+    // 再更新自己的所有子元素
+    for (var subObj of Object.values(this.__subObjects__)) {
+        // 计算子场景的进度
+        var percentage = this.__calculatePercentage__(subObj, rate, scrolled, gloalVars)
         
-        // 把 conf 里所有的属性都转移到对象中
-        for (var key of Object.keys(conf)) {
-            if (key !== "__onUpdate__" && key !== "__onInactive__" && key !== "__subObjects__") {
-                this[key] = conf[key]
-            }
-        }
-
-        for (var i = 2; i < arguments.length; ++i) {
-            this.addSubObject(arguments[i])
-        }
-
-        // 单独处理三个变量
-        if (conf instanceof SceneBase) {
-            this.__onUpdate__ = conf.__onUpdate__
-        } else if (conf.__onUpdate__) {
-            this.__onUpdateFunction__ = conf.__onUpdate__
-        }
-
-        if (conf instanceof SceneBase) {
-            this.__onInactive__ = conf.__onInactive__
-        } else if (conf.__onInactive__) {
-            this.__onInactiveFunction__ = conf.__onInactive__
-        }
-
-        if (conf.__subObjects__) {
-            if (Array.isArray(conf.__subObjects__)) {
-                for (var value of conf.__subObjects__) {
-                    this.addSubObject(value)
-                }
-            } else if (conf.__subObjects__ instanceof SceneBase) {
-                this.addSubObject(conf)
-            } else {
-                for (var key of Object.keys(conf.__subObjects__)) {
-                    this.addSubObject(conf.__subObjects__[key], key)
-                }
-            }
-        }
-    }
-
-    __isInActiveRange__(rate, scrolled, gloalVars) {
-        if (rate < this.__start__ || rate > this.__end__) {
-            return false
-        }
-        return true
-    }
-
-    __onUpdate__ (rate, scrolled, gloalVars) {
-        // 先自己更新
-        if (this.__onUpdateFunction__) {
-            this.__onUpdateFunction__.call(this, rate, scrolled, gloalVars)
-        }
-
-        // 再更新自己的所有子元素
-        for (var subObj of Object.values(this.__subObjects__)) {
-            // 计算子场景的进度
-            var percentage = (rate - subObj.__start__) / (subObj.__end__ - subObj.__start__)
-            
-            // 如果不在范围内
-            if (subObj.__isInActiveRange__(rate, scrolled, gloalVars) === false) {
-                if (subObj.isActive() === true) {
-                    if (subObj.__onInactive__) {
-                        subObj.__onInactive__.call(subObj, percentage, scrolled, gloalVars)
-                    }
-                    subObj.end()
-                }
-                
-                if (subObj.__onUpdateInactive__) {
-                    subObj.__onUpdateInactive__.call(subObj, percentage, scrolled, gloalVars)
-                }
-                continue
-            }
-            
-            // 调用子场景的更新函数
-            if (subObj.isActive() === false) {
-                if (subObj.__onActivate__) {
-                    subObj.__onActivate__.call(subObj, percentage, scrolled, gloalVars)
-                }
-                subObj.start()
-            }
-
-            if (subObj.__onUpdate__) {
-                subObj.__onUpdate__.call(subObj, percentage, scrolled, gloalVars)
-            }
-        }
-    }
-
-    __onInactive__ (rate, scrolled, gloalVars) {
-        // 先销毁漏网之鱼
-        for (var subObj of Object.values(this.__subObjects__)) {
+        // 如果不在范围内
+        if (subObj.__isInActiveRange__(rate, scrolled, gloalVars) === false) {
             if (subObj.isActive() === true) {
                 if (subObj.__onInactive__) {
-                    // 计算子场景的进度
-                    var percentage = (rate - subObj.__start__) / (subObj.__end__ - subObj.__start__)
                     subObj.__onInactive__.call(subObj, percentage, scrolled, gloalVars)
                 }
                 subObj.end()
             }
-        }
-
-        // 再销毁自己
-        if (this.__onInactiveFunction__) {
-            this.__onInactiveFunction__.call(this, rate, scrolled, gloalVars)
-        }
-    }
-
-    addSubObject (obj, id, father) {
-        if (id === undefined) {
-            if (SceneBase.__maxSubObjId__ === undefined) {
-                SceneBase.__maxSubObjId__ = 0
+            
+            if (subObj.__onUpdateInactive__) {
+                subObj.__onUpdateInactive__.call(subObj, percentage, scrolled, gloalVars)
             }
-            id = "__untitled" + (SceneBase.__maxSubObjId__++) + "__"
-        }
-        father = father ? father : this
-
-        if (obj.__father__ === undefined) {
-            obj.__father__ = father
+            continue
         }
         
-        this.__subObjects__[id] = new SceneBase(obj, father)
-        return id
-    }
-
-    delSubObject (id, rate, scrolled, gloalVars) {
-        if (this.__subObjects__[id] instanceof SceneBase && this.__subObjects__[id].isActive() === true) {
-            this.__subObjects__[id].__onInactive__(rate, scrolled, gloalVars)
+        // 调用子场景的更新函数
+        if (subObj.isActive() === false) {
+            if (subObj.__onActivate__) {
+                subObj.__onActivate__.call(subObj, percentage, scrolled, gloalVars)
+            }
+            subObj.start()
         }
-        delete this.__subObjects__[id]
+
+        if (subObj.__onUpdate__) {
+            subObj.__onUpdate__.call(subObj, percentage, scrolled, gloalVars)
+        }
+    }
+}
+
+SceneBase.prototype.__onInactive__ = function (rate, scrolled, gloalVars) {
+    // 先销毁漏网之鱼
+    for (var subObj of Object.values(this.__subObjects__)) {
+        if (subObj.isActive() === true) {
+            if (subObj.__onInactive__) {
+                // 计算子场景的进度
+                var percentage = (rate - subObj.__start__) / (subObj.__end__ - subObj.__start__)
+                subObj.__onInactive__.call(subObj, percentage, scrolled, gloalVars)
+            }
+            subObj.end()
+        }
     }
 
-    start() {
-        this.__isActive__ = true
+    // 再销毁自己
+    if (this.__onInactiveFunction__) {
+        this.__onInactiveFunction__.call(this, rate, scrolled, gloalVars)
+    }
+}
+
+SceneBase.prototype.__onUpdateInactive__ = function (rate, abso, gloalVars) {
+    if (this.__onUpdateInactiveFunction__) {
+        this.__onUpdateInactiveFunction__.call(this, rate, abso, gloalVars)
+    }
+}
+
+SceneBase.prototype.addSubObject = function (obj, id, father) {
+    if (this.__triggers__) {
+        for (var t of this.__triggers__) {
+            t.addSubObject(obj, id)
+        }
+    }
+    
+    if (id === undefined) {
+        if (SceneBase.__maxSubObjId__ === undefined) {
+            SceneBase.__maxSubObjId__ = 0
+        }
+        id = "__untitled" + (SceneBase.__maxSubObjId__++) + "__"
     }
 
-    end() {
-        this.__isActive__ = false
-    }
+    father = father ? father : this
 
-    isActive() {
-        return this.__isActive__
+    if (obj.__father__ === undefined) {
+        obj.__father__ = father
     }
+    
+    if (obj instanceof SceneBase) {
+        this.__subObjects__[id] = obj
+    } else {
+        this.__subObjects__[id] = new SceneBase(obj, father)
+    }
+    
+    return id
+}
+
+SceneBase.prototype.delSubObject = function (id, rate, scrolled, gloalVars) {
+    if (this.__subObjects__[id] instanceof SceneBase && this.__subObjects__[id].isActive() === true) {
+        this.__subObjects__[id].__onInactive__(rate, scrolled, gloalVars)
+    }
+    delete this.__subObjects__[id]
+}
+
+SceneBase.prototype.start = function () {
+    this.__isActive__ = true
+}
+
+SceneBase.prototype.end = function () {
+    this.__isActive__ = false
+}
+
+SceneBase.prototype.isActive = function () {
+    return this.__isActive__
 }
 
 SceneBase.newInstance = function () {
